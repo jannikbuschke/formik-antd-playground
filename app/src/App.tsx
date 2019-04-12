dd4ee7646e3fafa54de2c15ac6c1e43dda45c231
Jannik Buschke <jannik.buschke@gmail.com>
2019-04-13 00:40:15 +0200

    update sample

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -39,7 +39,10 @@ function App() {
           date: null
         }}
         onSubmit={(values, actions) => {
-          alert(JSON.stringify(values, null, 2));
+          notification.open({
+            duration: 0,
+            message: <pre>{JSON.stringify(values, null, 2)}</pre>
+          });
           actions.setSubmitting(false);
         }}
         validate={values => {
@@ -50,93 +53,79 @@ function App() {
             : undefined;
         }}
       >
-        <div className="split">
-          <div style={{ display: "grid", gridGap: 5 }}>
-            <h3>Buttons</h3>
+        <div
+          style={{
+            display: "flex",
+            marginLeft: "auto",
+            marginRight: "auto",
+            maxWidth: 800,
+            gridTemplateColumns: "1fr 1fr"
+          }}
+        >
+          <div
+            style={{
+              display: "grid",
+              gridGap: 5,
+              margin: 5,
+              height: "100%"
+            }}
+          >
             <Button.Group>
               <ResetButton>Reset</ResetButton>
               <SubmitButton>Submit (when dirty & valid)</SubmitButton>
               <SubmitButton disabled={false}>Submit</SubmitButton>
             </Button.Group>
-            <h3>Simple fields</h3>
-            <div className="field-container">
-              <span>Input</span>
-              <InputField name="email" />
-              <span>with props</span>
-              <InputField name="website" addonBefore="https://" />
-              <span>PasswordInput</span>
-              <InputPasswordField name="password" />
-              <span>TextArea</span>
-              <TextAreaField name="description" />
-              <span>InputNumber</span>
-              <InputNumberField name="index" />
-              <span>with formatter</span>
-              <InputNumberField
-                name="dollars"
-                formatter={(value: any) => `$ ${value}`}
-              />
-              <span>Checkbox</span>
-              <div>
-                <CheckboxField name="applyForNewsletter" />
-              </div>
-              <span>Switch</span>
-              <div>
-                <SwitchField name="applyForNewsletter" />
-              </div>
-              <span>DatePicker</span>
-              <DatePickerField name="date" />
-              <h3 style={{ gridColumnStart: 1, gridColumnEnd: 3 }}>Groups</h3>
-              <span>RadioGroup</span>
-              <RadioGroupField
-                name="radioGroup"
-                options={[
-                  { label: "Val 1", value: "val1" },
-                  { label: "5", value: 5 }
-                ]}
-              />
-              <span>CheckboxGroup</span>
-              <CheckboxGroupField
-                name="checkboxGroup"
-                options={[
-                  { label: "item 1", value: "1" },
-                  { label: "item 2", value: "2" }
-                ]}
-              />
-            </div>
+            <InputField name="email" />
+            <FormItem name="userName">
+              <InputField name="userName" />
+            </FormItem>
+            <InputField name="items[0]name" />
+            <InputField name="website" addonBefore="https://" />
+            <InputPasswordField name="password" />
+            <TextAreaField name="description" />
+            <InputNumberField name="index" />
+            <InputNumberField
+              name="dollars"
+              formatter={(value: any) => `$ ${value}`}
+            />
+            <CheckboxField name="applyForNewsletter">Newsletter</CheckboxField>
             <div>
-              <h3 style={{ gridColumnStart: 1, gridColumnEnd: 3 }}>Lists</h3>
-              <FormItem name="items[0]name">
-                <InputField name="items[0]name" />
-              </FormItem>
-              <h3 style={{ gridColumnStart: 1, gridColumnEnd: 3 }}>
-                Validation
-              </h3>
-              <FormItem name="userName">
-                <InputField name="userName" />
-              </FormItem>
-              <h3 style={{ gridColumnStart: 1, gridColumnEnd: 3 }}>
-                SelectField
-              </h3>
-              <SelectField name="select" style={{ width: "100%" }}>
-                {SelectField.renderOptions([
-                  { label: "displayed value", value: 2 },
-                  {
-                    label: <h1>hello world</h1>,
-                    value: "123"
-                  },
-                  {
-                    disabled: true,
-                    value: "123",
-                    label: "child"
-                  }
-                ])}
-              </SelectField>
+              <SwitchField name="applyForNewsletter" />
             </div>
+            <DatePickerField name="date" />
+            <RadioGroupField
+              name="radioGroup"
+              options={[
+                { label: "item 1", value: "1" },
+                { label: "item 2", value: 2 },
+                { label: "item 3", value: "2" }
+              ]}
+            />
+            <CheckboxGroupField
+              name="checkboxGroup"
+              options={[
+                { label: "item 1", value: "1" },
+                { label: "item 2", value: "2" },
+                { label: "item 3", value: "3" }
+              ]}
+            />
+            <SelectField name="select" style={{ width: "100%" }}>
+              {SelectField.renderOptions([
+                { label: "displayed value", value: 2 },
+                {
+                  label: <h1>hello world</h1>,
+                  value: "123"
+                },
+                {
+                  disabled: true,
+                  value: "123",
+                  label: "child"
+                }
+              ])}
+            </SelectField>
           </div>
-          <Divider type="vertical" style={{ height: "100%" }} />
-          <div>
-            <FormikDebug />
-          </div>
+
+          <FormikDebug style={{ maxWidth: 400 }} />
         </div>
       </Formik>
     </div>
