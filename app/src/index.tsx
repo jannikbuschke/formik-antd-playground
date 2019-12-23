502a8c35c947d2e906fbe5a5461034faae8ec795
Jannik Buschke <jannik.buschke@gmail.com>
2019-12-23 22:32:56 +0100

    add react-router and enable tab navigation

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { render } from "react-dom";
+import * as React from "react"
+import { render } from "react-dom"
 import {
   Checkbox,
   Input,
@@ -19,14 +19,20 @@ import {
   Cascader,
   TreeSelect,
   Transfer,
-  Form
-} from "formik-antd";
-import { Formik, FieldArray } from "formik";
-import { Button, notification, Icon, TreeSelect as $TreeSelect, Tabs, DatePicker as $DatePicker } from "antd";
-import { Header } from "./Header";
-import "./index.css";
-import { generatePassword } from "./utils";
-import { SampleForm } from "./SampleForm";
+  Form,
+} from "formik-antd"
+import { Formik } from "formik"
+import { Button, notification, Icon, Tabs } from "antd"
+import { Header } from "./Header"
+import "./index.css"
+import { generatePassword } from "./utils"
+import { SampleForm } from "./SampleForm"
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch as BrowserSwitch,
+} from "react-router-dom"
+import { useParams, useHistory } from "react-router"
 
 function App() {
   return (
@@ -46,182 +52,234 @@ function App() {
         city: 3,
         radioGroup: "1",
         todos: ["2", "3"],
-        "slider1": 30,
-        "slider2": [20, 60]
+        slider1: 30,
+        slider2: [20, 60],
+        select1: 2,
       }}
       onSubmit={(values, actions) => {
         notification.open({
           duration: 0,
-          message: <pre>{JSON.stringify(values, null, 2)}</pre>
-        });
-        actions.setSubmitting(false);
+          message: <pre>{JSON.stringify(values, null, 2)}</pre>,
+        })
+        actions.setSubmitting(false)
       }}
-      validate={values => {
+      validate={(values) => {
         return !values.userName
           ? {
-            userName: "required"
-          }
-          : undefined;
+              userName: "required",
+            }
+          : undefined
       }}
-      render={() => <Form>
-        <div className="container">
-          <div className="component-container">
-            <Input name="email" />
-            <FormItem name="userName">
-              <Input name="userName" />
-            </FormItem>
-            <Input addonBefore="city" name="address.city" />
-            <Input.Password name="password" />
-            <Input.TextArea name="description" />
-            <AutoComplete
-              name="auto"
-              placeholder="Autocomplete"
-              dataSource={["Berlin", "Amsterdam", "Paris"]}
-              showArrow={true}
-              allowClear={true}
-            />
-            <Rate name="rate" allowHalf={true} allowClear={true} />
-            <Rate
-              name="rate"
-              allowHalf={true}
-              character={<Icon type="heart" />}
-              style={{ color: "red", }}
-              allowClear={true}
-            />
-            <Slider name="slider1" />
-            <Slider name="slider2" range={true} />
-            <InputNumber name="index" min={0} />
-            <InputNumber
-              name="dollars"
-              formatter={(value: any) => `$ ${value}`}
-            />
-            <Checkbox name="newsletter">Newsletter</Checkbox>
-            <div>
-              <Switch name="consent" />
-            </div>
-            <TimePicker name="time" />
-            <DatePicker name="date" showTime={true} />
-            <DatePicker.RangePicker name="range" placeholder={["Rangepicker", "Rangepicker"]} />
-            <DatePicker.WeekPicker name="week" placeholder="WeekPicker" />
-            <DatePicker.MonthPicker name="month" placeholder="MonthPicker" />
-            <Radio.Group
-              name="radioGroup"
-              options={[
-                { label: "item 1", value: "1" },
-                { label: "item 2", value: 2 },
-                { label: "item 3", value: "3" },
-                { label: <span>foo</span>, value: "4" }
-              ]}
-            />
-            <Radio.Group name="city" size="default">
-              <Radio.Button value={1}>Hamburg</Radio.Button>
-              <Radio.Button value={2}>Amsterdam</Radio.Button>
-              <Radio.Button value={3}>London</Radio.Button>
-            </Radio.Group>
-            <Checkbox.Group
-              name="todos"
-              options={[
-                { label: "item 1", value: "1" },
-                { label: "item 2", value: "2" },
-                { label: "item 3", value: "3" }
-              ]}
-            />
-            <Select name="select1" style={{ width: "100%" }} placeholder="Simple select">
-              <Select.Option value={1}>item 1</Select.Option>
-              <Select.Option value={2}>item 2</Select.Option>
-              <Select.Option value={3}>item 3</Select.Option>
-            </Select>
-            <Select name="select2" style={{ width: "100%" }} placeholder="Select multiple" mode="multiple">
-              <Select.Option value={1}>item 1</Select.Option>
-              <Select.Option value={2}>item 2</Select.Option>
-              <Select.Option value={3}>item 3</Select.Option>
-            </Select>
-            <Select name="select3" style={{ width: "100%" }} placeholder="Select with groups">
-              <Select.OptGroup label="group 1">
+      render={() => (
+        <Form>
+          <div className="container">
+            <div className="component-container">
+              <Input name="email" />
+              <FormItem name="userName">
+                <Input name="userName" />
+              </FormItem>
+              <Input addonBefore="city" name="address.city" />
+              <Input.Password name="password" />
+              <Input.TextArea name="description" />
+              <AutoComplete
+                name="auto"
+                placeholder="Autocomplete"
+                // defaultValue="1"
+                // dataSource={["Berlin", "Amsterdam", "Paris"]}
+                dataSource={[
+                  { value: "1", text: "Berlin" },
+                  { value: "2", text: "Amsterdam" },
+                  { value: "3", text: "Paris" },
+                ]}
+                showArrow={true}
+                allowClear={true}
+              />
+              <Rate name="rate" allowHalf={true} allowClear={true} />
+              <Rate
+                name="rate"
+                allowHalf={true}
+                character={<Icon type="heart" />}
+                style={{ color: "red" }}
+                allowClear={true}
+              />
+              <Slider name="slider1" />
+              <Slider name="slider2" range={true} />
+              <InputNumber name="index" min={0} />
+              <InputNumber
+                name="dollars"
+                formatter={(value: any) => `$ ${value}`}
+              />
+              <Checkbox name="newsletter">Newsletter</Checkbox>
+              <div>
+                <Switch name="consent" />
+              </div>
+              <TimePicker name="time" />
+              <DatePicker name="date" showTime={true} />
+              <DatePicker.RangePicker
+                name="range"
+                placeholder={["Rangepicker", "Rangepicker"]}
+              />
+              <DatePicker.WeekPicker name="week" placeholder="WeekPicker" />
+              <DatePicker.MonthPicker name="month" placeholder="MonthPicker" />
+              <Radio.Group
+                name="radioGroup"
+                options={[
+                  { label: "item 1", value: "1" },
+                  { label: "item 2", value: 2 },
+                  { label: "item 3", value: "3" },
+                  { label: <span>foo</span>, value: "4" },
+                ]}
+              />
+              <Radio.Group name="city" size="default">
+                <Radio.Button value={1}>Hamburg</Radio.Button>
+                <Radio.Button value={2}>Amsterdam</Radio.Button>
+                <Radio.Button value={3}>London</Radio.Button>
+              </Radio.Group>
+              <Checkbox.Group
+                name="todos"
+                options={[
+                  { label: "item 1", value: "1" },
+                  { label: "item 2", value: "2" },
+                  { label: "item 3", value: "3" },
+                ]}
+              />
+              <Select
+                name="select2"
+                style={{ width: "100%" }}
+                placeholder="Select multiple"
+                mode="multiple"
+              >
                 <Select.Option value={1}>item 1</Select.Option>
                 <Select.Option value={2}>item 2</Select.Option>
                 <Select.Option value={3}>item 3</Select.Option>
-              </Select.OptGroup>
-              <Select.OptGroup label="group 2">
-                <Select.Option value={4}>item 4</Select.Option>
-                <Select.Option value={5}>item 5</Select.Option>
-                <Select.Option value={6}>item 6</Select.Option>
-              </Select.OptGroup>
-            </Select>
-            <Select name="select4" style={{ width: "100%" }} placeholder="Select with renderOptions() helper">
-              {Select.renderOptions([
-                { label: "foo" },
-                { label: <h3>bar</h3>, value: 2 },
-                { disabled: true, label: "baz" }
-              ])}
-            </Select>
-            <Cascader options={[{
-              value: 'zhejiang',
-              label: 'Zhejiang',
-              children: [{
-                value: 'hangzhou',
-                label: 'Hangzhou',
-                children: [{
-                  value: 'xihu',
-                  label: 'West Lake',
-                }],
-              }],
-            }, {
-              value: 'jiangsu',
-              label: 'Jiangsu',
-              children: [{
-                value: 'nanjing',
-                label: 'Nanjing',
-                children: [{
-                  value: 'zhonghuamen',
-                  label: 'Zhong Hua Men',
-                }],
-              }],
-            }]}
-              name="cascader"
-              placeholder="Cascader"
-            />
-            <TreeSelect name="treeselect" placeholder="Treeselect" >
-              <TreeSelect.TreeNode value="parent 1" title="parent 1" key="0-1">
-                <TreeSelect.TreeNode value="parent 1-0" title="parent 1-0" key="0-1-1">
-                  <TreeSelect.TreeNode value="leaf1" title="my leaf" key="random" />
-                  <TreeSelect.TreeNode value="leaf2" title="your leaf" key="random1" />
+              </Select>
+              <Select
+                name="select3"
+                style={{ width: "100%" }}
+                placeholder="Select with groups"
+              >
+                <Select.OptGroup label="group 1">
+                  <Select.Option value={1}>item 1</Select.Option>
+                  <Select.Option value={2}>item 2</Select.Option>
+                  <Select.Option value={3}>item 3</Select.Option>
+                </Select.OptGroup>
+                <Select.OptGroup label="group 2">
+                  <Select.Option value={4}>item 4</Select.Option>
+                  <Select.Option value={5}>item 5</Select.Option>
+                  <Select.Option value={6}>item 6</Select.Option>
+                </Select.OptGroup>
+              </Select>
+              <Cascader
+                options={[
+                  {
+                    value: "zhejiang",
+                    label: "Zhejiang",
+                    children: [
+                      {
+                        value: "hangzhou",
+                        label: "Hangzhou",
+                        children: [
+                          {
+                            value: "xihu",
+                            label: "West Lake",
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ]}
+                name="cascader"
+                placeholder="Cascader"
+              />
+              <TreeSelect name="treeselect" placeholder="Treeselect">
+                <TreeSelect.TreeNode
+                  value="parent 1"
+                  title="parent 1"
+                  key="0-1"
+                >
+                  <TreeSelect.TreeNode
+                    value="parent 1-0"
+                    title="parent 1-0"
+                    key="0-1-1"
+                  >
+                    <TreeSelect.TreeNode
+                      value="leaf1"
+                      title="my leaf"
+                      key="random"
+                    />
+                    <TreeSelect.TreeNode
+                      value="leaf2"
+                      title="your leaf"
+                      key="random1"
+                    />
+                  </TreeSelect.TreeNode>
+                  <TreeSelect.TreeNode
+                    value="parent 1-1"
+                    title="parent 1-1"
+                    key="random2"
+                  >
+                    <TreeSelect.TreeNode
+                      value="sss"
+                      title={<b style={{ color: "#08c" }}>sss</b>}
+                      key="random3"
+                    />
+                  </TreeSelect.TreeNode>
                 </TreeSelect.TreeNode>
-                <TreeSelect.TreeNode value="parent 1-1" title="parent 1-1" key="random2">
-                  <TreeSelect.TreeNode value="sss" title={<b style={{ color: '#08c' }}>sss</b>} key="random3" />
-                </TreeSelect.TreeNode>
-              </TreeSelect.TreeNode>
-            </TreeSelect>
-            <Transfer
-              name="transfer"
-              dataSource={[{ key: "1", title: "item 1", }, { key: "2", title: "item 2" }, { key: "3", title: "item 3", }]}
-              render={(item: any) => item.title}
-            />
-            <Button.Group size="large">
-              <ResetButton>Reset</ResetButton>
-              <SubmitButton type="primary" disabled={false}>
-                Submit
-            </SubmitButton>
-            </Button.Group>
-          </div>
+              </TreeSelect>
+              <Transfer
+                name="transfer"
+                dataSource={[
+                  { key: "1", title: "item 1" },
+                  { key: "2", title: "item 2" },
+                  { key: "3", title: "item 3" },
+                ]}
+                render={(item: any) => item.title}
+              />
+              <Button.Group size="large">
+                <ResetButton>Reset</ResetButton>
+                <SubmitButton type="primary" disabled={false}>
+                  Submit
+                </SubmitButton>
+              </Button.Group>
+            </div>
 
-          <FormikDebug style={{ maxWidth: 400 }} />
-        </div>
-      </Form>} />
-  );
+            <FormikDebug style={{ maxWidth: 400 }} />
+          </div>
+        </Form>
+      )}
+    />
+  )
 }
 
-const rootElement = document.getElementById("root");
+function Root() {
+  const { tab } = useParams<{ tab: string | undefined }>()
+  const history = useHistory()
+  return (
+    <Tabs
+      animated={false}
+      onChange={(key) => {
+        history.push(key)
+      }}
+      activeKey={tab || "/"}
+    >
+      <Tabs.TabPane tab="Overview" key={"/"}>
+        <Header />
+        <App />
+      </Tabs.TabPane>
+      <Tabs.TabPane tab="Sample Form" key={"form"}>
+        <SampleForm />
+      </Tabs.TabPane>
+    </Tabs>
+  )
+}
+
+const rootElement = document.getElementById("root")
 render(
-  <Tabs defaultActiveKey="1">
-    <Tabs.TabPane tab="Overview" key="1">
-      <Header />
-      <App />
-    </Tabs.TabPane>
-    <Tabs.TabPane tab="Sample Form" key="2">
-      <SampleForm />
-    </Tabs.TabPane>
-    {/* <AgendaItemMasterDetail /> */}
-  </Tabs>
-  , rootElement
-);
+  <Router>
+    <BrowserSwitch>
+      <Route path="/" exact={true} component={Root} />
+      <Route path="/:tab" component={Root} />
+    </BrowserSwitch>
+  </Router>,
+  rootElement,
+)
