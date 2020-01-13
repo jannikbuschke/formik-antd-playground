a8cb5d6f0bd55be60716e499a7da564424afbd1a
Jannik Buschke <jannik.buschke@gmail.com>
2020-01-14 00:28:13 +0100

    remove app

--- app/src/utils.ts
+++
@@ -1,6 +0,0 @@
-// this is not necessarily a good password generator
-export function generatePassword() {
-  return new Array(20)
-    .join()
-    .replace(/(.|$)/g, () => ((Math.random() * 36) | 0).toString(36));
-}
